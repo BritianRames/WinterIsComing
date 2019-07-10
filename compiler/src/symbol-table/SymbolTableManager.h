3a40e05adbbd397d05fef059c07cf5c7aa4a0ffb
David Alberto Medina Medina <david00medina@gmail.com>
2019-07-10 03:09:12 +0100

    [+] Refractor

--- compiler/src/symbol-table/SymbolTableManager.h
+++ compiler/src/symbol-table/SymbolTableManager.h
@@ -3,6 +3,19 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+struct Symbol
+{
+    char *id;
+    int address; //absolute or relative
+    char type;
+    int scope;
+    int label;
+    int numberOfParameters;
+    int numberOfLocalVariables;
+    struct Symbol *previousSymbol;
+    struct Symbol *nextSymbol;
+};
+
 void insertVariableInSymbolTable(char *id);
 void insertParameterInSymbolTable(char *id);
 void insertFunctionSymbolTable(char *id);
@@ -20,17 +33,3 @@ void closeScopeInSymbolTable();
 void _removeLocalVariablesFromSymbolTable();
 int _getLabel();
 void printSymbolTable();
-
-
-struct Symbol
-{
-    char *id;
-    int address; //absolute or relative
-    char type;
-    int scope;
-    int label;
-    int numberOfParameters;
-    int numberOfLocalVariables;
-    struct Symbol *previousSymbol;
-    struct Symbol *nextSymbol;
-};
