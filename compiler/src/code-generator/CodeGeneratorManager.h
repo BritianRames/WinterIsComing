2145fd73dbf8eb40d473e49d77292cd998f5c7c6
ale <alerodriguezangulo@gmail.com>
2019-07-12 18:57:19 +0100

    Refactor

--- compiler/src/code-generator/CodeGeneratorManager.h
+++ compiler/src/code-generator/CodeGeneratorManager.h
@@ -10,3 +10,18 @@ void generateFunctionReturnValueCode(int value);
 void generateFunctionReturnVariableCode(char* variableToReturn);
 void generateAssignValueToLocalVariableAssignationCode(char *local_variable_id, int value);
 void generateAssignLocalVariableToLocalVariableCode(char* local_variable_id, char value_id);
+void printReturnVariable();
+
+#include "../symbol-table/SymbolTableManager.h"
+#include "CodeGeneratorManager.h"
+#include "StackManager.h"
+#include "CodePrinter.h"
+
+void generateAssignValueToGlobalVariable(char* variable_id, int value);
+void generateFunctionHeaderCode();
+void generateAssignVariableToGlobalVariableCode(char* variable_id, char value_id);
+void generateFunctionReturnValueCode(int value);
+void generateFunctionReturnVariableCode(char* variableToReturn);
+void generateAssignValueToVariableAssignationCode(char *variable_id, int value);
+void generateAssignVariableToVariableCode(char* variable_id, char value_id);
+int _getVariableAddress(char* variable_id);
